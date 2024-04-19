cf12b236048380f3a39782fab0330687ec1c9608
AhmedNagii <ahmednagihassan09@gmail.com>
2024-04-19 16:49:32 +0200

    add basic routs

---
+++ src/components/About.jsx
@@ -0,0 +1,4 @@
+function About() {
+  return <h1>About page goes here! 🎉</h1>;
+}
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,4 @@
+function Home() {
+  return <h1>Hello, React Router!</h1>;
+}
+export default Home
